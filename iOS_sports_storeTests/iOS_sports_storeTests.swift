35b7d721a03179ccfa28878d93d32842e3053618
김광호 <gwangho.kim@navercorp.com>
2015-03-02 12:49:55 +0900

    Initial Commit

---
+++ iOS_sports_storeTests/iOS_sports_storeTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_sports_storeTests.swift
+//  iOS_sports_storeTests
+//
+//  Created by 김광호 on 2015. 3. 2..
+//  Copyright (c) 2015년 icelancer. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOS_sports_storeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
